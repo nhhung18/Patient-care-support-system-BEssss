1eaced2897a4187372332f075b75ef02e31edc98
nhhung18 <nguyenhung18042005@gmail.com>
2026-04-15 05:11:19 +0700

    base of project

---
+++ src/main/java/com/se302_group2/PatientCareSupport/PatientCareSupportApplication.java
@@ -0,0 +1,13 @@
+package com.se302_group2.PatientCareSupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientCareSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientCareSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se302_group2/PatientCareSupport/PatientCareSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se302_group2.PatientCareSupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientCareSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
